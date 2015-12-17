8a9cb501162185108e63209ca5a80a4e9baff351
Mjx <majixian@bjtu.edu.cn>
2015-12-17 15:16:06 +0800

    framework

---
+++ Tool/xmlWithArray.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mjx
+ * Date: 2015/12/17
+ * Time: 15:08
+ */
+class xmlWithArray{
+
+    public static function xmlToArray($xml){
+
+    }
+
+    public static function arrayToXml($array){
+
+    }
+
+
+}
---
+++ xmlWithArrayTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mjx
+ * Date: 2015/12/17
+ * Time: 15:12
+ */
+
+class Test{
+
+    public static function main(){
+
+    }
+
+}
+
